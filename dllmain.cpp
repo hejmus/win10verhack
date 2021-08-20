a9735a15262e6961a822edb8268af075965bc2ce
hejmus <krzysiek@hejmowski.pl>
2021-08-21 01:45:16 +0200

    pretend to be windows10

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,19 +1,49 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+static BOOL(WINAPI* TrueVerifyVersionInfoW)(
+    _Inout_ LPOSVERSIONINFOEXW lpVersionInformation,
+    _In_    DWORD dwTypeMask,
+    _In_    DWORDLONG dwlConditionMask) = VerifyVersionInfoW;
+
+BOOL
+WINAPI
+VerifyVersionInfoW_Win10(
+    _Inout_ LPOSVERSIONINFOEXW lpVersionInformation,
+    _In_    DWORD dwTypeMask,
+    _In_    DWORDLONG dwlConditionMask
+)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
+    if (lpVersionInformation->dwMajorVersion == 10)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinst, DWORD dwReason, LPVOID reserved)
+{
+    LONG error;
+    (void)hinst;
+    (void)reserved;
+
+    if (DetourIsHelperProcess()) {
+        return TRUE;
+    }
+
+    if (dwReason == DLL_PROCESS_ATTACH) {
+        DetourRestoreAfterWith();
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourAttach(&(PVOID&)TrueVerifyVersionInfoW, VerifyVersionInfoW_Win10);
+        error = DetourTransactionCommit();
+    }
+    else if (dwReason == DLL_PROCESS_DETACH) {
+        DetourTransactionBegin();
+        DetourUpdateThread(GetCurrentThread());
+        DetourDetach(&(PVOID&)TrueVerifyVersionInfoW, VerifyVersionInfoW_Win10);
+        error = DetourTransactionCommit();
     }
     return TRUE;
 }
 
+//
+///////////////////////////////////////////////////////////////// End of File.
